175466247a93ea0011d785c8031ec214e5605923
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-24 22:26:08 +0000

    fixed scrollg

--- src/renderer/components/Viewer.js
+++ src/renderer/components/Viewer.js
@@ -6,7 +6,7 @@ export default class Viewer extends Component {
     render() {
         return (
             <div className="Viewer" onKeyPress={this.props.onKeyPress}>
-                <Content src={this.props.src} type={this.props.type} />
+                <Content src={this.props.src} type={this.props.type} zoom={this.props.zoom} x={this.props.x} y={this.props.y} />
                 <Likes amount={this.props.likes} />
             </div>
         );
