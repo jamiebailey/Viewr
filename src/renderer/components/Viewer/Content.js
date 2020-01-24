768115d129561437720e33184c45f19be1e2e48e
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-24 23:07:34 +0000

    basic zoom works

--- src/renderer/components/Viewer/Content.js
+++ src/renderer/components/Viewer/Content.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Content extends Component {
+    
+    state = {
+        zoom: 1,
+        x: 0,
+        y: 0
+    };
+
+    componentDidMount() {
+        window.addEventListener('wheel', this.onWheel.bind(this));
+    }
+
+    onWheel(e) {
+        let change = -e.deltaY * 0.0005;
+        let elem = this.refs.Content;
+        let winW = window.innerWidth;
+        let winH = window.innerHeight;
+        this.setState(prevState => {
+            return {
+                zoom: prevState.zoom + change,
+                x: prevState.x - (winW * change) / 2,
+                y: prevState.y - (winH * change) / 2
+            }
+        });
+    }
+
     render() {
         const src = this.props.src;
         const type = this.props.type;
-        const zoom = this.props.zoom ?? 1;
-        const x = this.props.x ?? 0;
-        const y = this.props.y ?? 0;
+        const zoom = this.state.zoom ?? 1;
+        const x = this.state.x ?? 0;
+        const y = this.state.y ?? 0;
 
         let style = {};
         style.width = (100 * zoom) + '%';
