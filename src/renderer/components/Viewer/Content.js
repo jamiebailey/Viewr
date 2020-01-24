7f975ac6c96209c67d8eee47d470e9b813f3c9d1
Jamie Bailey <jamie.bailey.dev@gmail.com>
2020-01-24 22:24:39 +0000

    fixed resizing

--- src/renderer/components/Viewer/Content.js
+++ src/renderer/components/Viewer/Content.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Content extends Component {
-    componentDidMount() {
-        let winX = window.innerWidth;
-        let winY = window.innerHeight;
-    }
-
     render() {
         const src = this.props.src;
         const type = this.props.type;
+        const zoom = this.props.zoom ?? 1;
+        const x = this.props.x ?? 0;
+        const y = this.props.y ?? 0;
+
+        let style = {};
+        style.width = (100 * zoom) + '%';
+        style.height = (100 * zoom) + '%';
+        style.left = x;
+        style.top = y;
         let output = null;
         if(type === 'image') {
             output = <img src={src}></img>
@@ -16,7 +20,7 @@ export default class Content extends Component {
             output = <video src={src} autoPlay="autoplay" loop={true}></video>
         }
         return (
-        <div className="Content">{output}</div>
+        <div className="Content" ref="Content" style={style}>{output}</div>
         );
     }
 }
